de5bd615f8ad8bb287ca2d6527c6749f727b63b8
Mykola <nyr3mvneov@gmail.com>
2026-03-22 18:17:38 +0200

    task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+if (process.argv.length !== 4) {
+  console.error('Wrong number of arguments');
+} else {
+  const file1 = process.argv[2];
+  const destination = process.argv[3];
+
+  if (!fs.existsSync(file1)) {
+    console.error(`File ${file1} does not exist`);
+  } else {
+    const sourcePath = path.resolve(file1);
+    let targetPath = destination;
+
+    if (fs.existsSync(destination)) {
+      const destStat = fs.statSync(destination);
+
+      if (destStat.isDirectory()) {
+        targetPath = path.join(destination, path.basename(file1));
+      }
+    } else {
+      const destinationDir = path.dirname(destination);
+
+      const missingDestinationDir = !fs.existsSync(destinationDir);
+      const invalidDestinationDir =
+        !missingDestinationDir && !fs.statSync(destinationDir).isDirectory();
+
+      if (missingDestinationDir || invalidDestinationDir) {
+        console.error(`Destination directory ${destinationDir} does not exist`);
+        targetPath = null;
+      }
+    }
+
+    if (targetPath && sourcePath !== path.resolve(targetPath)) {
+      try {
+        fs.renameSync(file1, targetPath);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
